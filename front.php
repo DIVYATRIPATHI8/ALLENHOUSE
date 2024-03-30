6ddea27b3596d40c3022b94ba90ae1fe931fe532
DIVYATRIPATHI8 <165458452+DIVYATRIPATHI8@users.noreply.github.com>
2024-03-30 12:09:15 +0530

    FRONT ADDED

---
+++ front.php
@@ -0,0 +1,162 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav><img src="code/logo.jpeg" alt="logo">
+        <div class="navigation">
+            <ul><i id="menu-close" class="fa fa-window-close-o" aria-hidden="true"></i>
+                
+                <li><a href="code/registration.php">REGISTRATION</a></li>
+                <li><a href="https://allenhouse.ac.in/#">ABOUT </a></li>
+                <li><a href="code/events2.php">EVENTS</a></li>
+                <li><a href="code/CLUB.php">CLUBS</a></li>
+                <li><a href="code/code/placement2.php">PLACEMENT</a></li>
+                
+                
+            </ul>
+            <img  id="menu-btn"  src="code/menu.png">
+        </div>
+    </nav>
+    <!--home-->
+    <section id="home">
+        <h2>
+            ENHANCE YOUR FUTURE WITH ALLENHOUSE </h2>
+        <P>Allenhouse Group of Institutions is one of India's top-ranked college for master's degree and bachelor's
+            degree.</P>
+        <div class="btn">
+            <a class="blue" href="https://allenhouse.ac.in/">learn more</a>
+            <a class="yellow" href="https://allenhouse.ac.in/">visit courses</a>
+        </div>
+    </section>
+    <!--courses-->
+    <section id="course">
+        <h1> OUR POPULAR COURSES</h1>
+        <p> “Education is the passport to the future, for tomorrow belongs to those who prepare for it today.” —Malcolm
+            X
+
+        </p>
+        <DIV CLASS="COURSE-BOX">
+            <DIV CLASS="COURSES">
+                <IMG SRC="code/Allen_Logo.png">
+                <DIV class="DETAIL">
+                    <H6>B.TECH AND DEPLOMA</H6>
+                    <div class="star">
+                        <i class=" fas fa-star"></i>
+
+
+                    </div>
+                </DIV>
+            </DIV><BR><bR>
+            <DIV CLASS="COURSES">
+                <IMG SRC="code/ABS LOGO.png" height="120PX" width="420PX">
+                <DIV class="DETAIL">
+                    <H6><B>BBA AND BCA</B></H6>
+                    <div class="star">
+                        <i class=" fas fa-star"></i>
+
+
+                    </div>
+                </DIV>
+            </DIV>
+            <BR><bR>
+            <DIV CLASS="COURSES">
+                <IMG SRC="code/AIM-Logo.jpg " height="120PX" width="400PX">
+                <DIV class="DETAIL">
+                    <H6><B>MBA</B></H6>
+                    <div class="star">
+                        <i class=" fas fa-star"></i>
+
+
+                    </div>
+                </DIV>
+            </DIV>
+
+        </DIV>
+    </section>
+    <hr>
+    <!--PROFILES-->
+    <SECTION ID="EXPERTS">
+        <H1> RESPECTED DIRECTORS AND HONOURABLE DEANS </H1><br><br>
+        <P></P>
+        <DIV class="EXPERT-BOX">
+            <DIV CLASS="PROFILE">
+                <IMG SRC="code/DR BHGWAN.jfif">
+                <H6>DR.BHAGWAN JAGWANI</H6>
+                <P>Campus Director, Allenhouse Group of Institutions</P>
+                <div id="PRO-LINKS">
+                    <I class=" fa fa-facebook-f"></I>
+                    <I class=" fa fa-instagarm"></I>
+                    <I class=" fa fa-linkedin"></I>
+                </div>
+            </DIV>
+            <DIV CLASS="PROFILE">
+                <IMG SRC="code/DR CHAWALA.jfif">
+                <H6>DR.RUBBY CHAWLA</H6>
+                <P>Director, Allenhouse Group of Institutions</P>
+                <div id="PRO-LINKS">
+                    <I class=" fa fa-facebook-f"></I>
+                    <I class=" fa fa-instagarm"></I>
+                    <I class=" fa fa-linkedin"></I>
+                </div>
+            </DIV>
+            <DIV CLASS="PROFILE">
+                <IMG SRC="code/DR DEV SINGH.jpg">
+                <H6>DR.DEV SINGH</H6>
+                <P>DEAN ACADMICS</P>
+                <div id="PRO-LINKS">
+                    <I class=" fa fa-facebook-f"></I>
+                    <I class=" fa fa-instagarm"></I>
+                    <I class=" fa fa-linkedin"></I>
+                </div>
+                <DIV CLASS="PROFILE">
+                    <IMG SRC="code/DR DEEPIKA PRASADI AALENHOSUE.jpg">
+                    <H6>DR.DEEPIKA PRASADI</H6>
+                    <P>DEAN STUDENT WELFARE</P>
+                    <div id="PRO-LINKS">
+                        <I class=" fa fa-facebook-f"></I>
+                        <I class=" fa fa-instagarm"></I>
+                        <I class=" fa fa-linkedin"></I>
+                    </div>
+                </DIV>
+            </DIV>
+        </DIV>
+    </SECTION>
+    <footer>
+        <div id="footer-col">
+            <h3>Contact Us</h3>
+            <li>Plot No.- 176, Kulgaon Road, Rooma,
+                 Kanpur – 208008, Uttar Pradesh</li>
+            <li>Phone: 8127505222, 8127405222</li>
+            <li>Email: info@allenhouse.ac.in </li>
+            <li><a
+                    href="https://www.google.com/maps?ll=26.36555,80.433566&z=14&t=m&hl=en&gl=IN&mapclient=embed&q=allenhouse+colleges">location</a>
+            </li>
+        </div>
+        <div id="footer-col">
+            <h3>our colleges</h3>
+            <li><a href="https://allenhouseinstituteoftechnology.in/" >ALLENHOUSE INSTITUTE OF TECHNOLOGY</a></li>
+            <li><A HREF="https://abss.ac.in/" >ALLENHOUSE BUSSINESS SCHOOL</A> </li>
+            <li><A HREF="https://allenhouseinstituteoftechnology.in/aim/">ALLENHOSUE INSTITUTE OF MANAGMENT</A></li>
+        </div>
+        <div id="footer-col">
+            <h3>USEFUL LINK</h3>
+            <li><a href="https://allenhouseinstituteoftechnology.in/wp-content/uploads/2022/03/Mandatory-Disclosure.pdf" >AICTE DISCLOSURE</a></li>
+            <LI><A HREF="https://allenhouseinstituteoftechnology.in/aicte-eoa-aktu-eoa/">EOA-AICTE & AKTU</A></LI>
+            <LI><A  HREF="https://www.aicte-india.org/feedback/">AICTE FEEDBACK</A></LI>
+            <LI><A HREF="https://allenhouseinstituteoftechnology.in/">FACULTY ACADEMIC</A></LI>
+            <LI><A HREF="https://allenhouseinstituteoftechnology.in/wp-content/uploads/2022/05/Statutory-Decration-Under-the-RTI-Act-2005.pdf">STATUTORY DECRATION</A></LI>
+      </div>
+
+    </footer>
+</body>
+
+</html>
